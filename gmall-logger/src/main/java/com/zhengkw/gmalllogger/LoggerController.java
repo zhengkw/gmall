8a9988fbc95d11710f9b1520b9bd75de6694e01b
zhengkw <swimer@vip.qq.com>
2020-05-28 13:48:38 +0800

    commit

---
+++ gmall-logger/src/main/java/com/zhengkw/gmalllogger/LoggerController.java
@@ -0,0 +1,12 @@
+package com.zhengkw.gmalllogger;
+
+/**
+ * @ClassName:LoggerController
+ * @author: zhengkw
+ * @description: 日志服务器
+ * @date: 20/05/28下午 1:46
+ * @version:1.0
+ * @since: jdk 1.8
+ */
+public class LoggerController {
+}
