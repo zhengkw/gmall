af7968055c9b236b778f8802af0f0f2fbc0c8594
zhengkw <swimer@vip.qq.com>
2020-05-28 20:33:58 +0800

    utils

---
+++ gmall-common/src/main/java/com/zhengkw/common/Constant.java
@@ -0,0 +1,14 @@
+package com.zhengkw.common;
+
+/**
+ * @ClassName:Constant
+ * @author: zhengkw
+ * @description: 常量
+ * @date: 20/05/28下午 7:57
+ * @version:1.0
+ * @since: jdk 1.8
+ */
+public class Constant {
+    public static final String START_TOPIC = "start_topic";
+    public static final String EVENT_TOPIC = "event_topic";
+}
---
+++ gmall-common/src/main/java/com/zhengkw/common/util/PorpertiesUtil.java
@@ -0,0 +1,59 @@
+package com.zhengkw.common.util;
+
+import javax.lang.model.element.VariableElement;
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.PrivateKey;
+import java.util.Properties;
+
+/**
+ * @ClassName:PorpertiesUtil
+ * @author: zhengkw
+ * @description: properties文件读取工具类
+ * @date: 20/05/28下午 8:03
+ * @version:1.0
+ * @since: jdk 1.8
+ */
+public class PorpertiesUtil {
+    private String value = "";
+    private static Properties props;
+
+    /**
+     * @param propertyName 属性名
+     * @param fileName     文件名
+     * @descrption: 获取属性值
+     * @return: java.lang.String
+     * @date: 20/05/28 下午 8:27
+     * @author: zhengkw
+     */
+    public String getProperty(String propertyName, String fileName) {
+        props = new Properties();
+        InputStream in = null;
+        try {
+            //方式1
+            //通过类加载器获取properties文件流
+          //  in = PorpertiesUtil.class.getClassLoader().getResourceAsStream(fileName);
+            //方式2
+            //通过类进行获取properties文件流
+            in = PorpertiesUtil.class.getResourceAsStream(fileName);
+            //加载文件
+            props.load(in);
+            //获取属性值！
+            value = props.getProperty(propertyName);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (in != null) {
+                try {
+                    in.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return value;
+    }
+}
