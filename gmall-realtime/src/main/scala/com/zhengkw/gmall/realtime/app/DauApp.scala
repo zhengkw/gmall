231b0f477acd26cd1bb1bac3218acd72380fee31
zhengkw <swimer@vip.qq.com>
2020-05-28 22:04:15 +0800

    app

--- gmall-realtime/src/main/scala/com/zhengkw/gmall/realtime/app/DauApp.scala
+++ gmall-realtime/src/main/scala/com/zhengkw/gmall/realtime/app/DauApp.scala
@@ -1,5 +1,6 @@
 package com.zhengkw.gmall.realtime.app
 
+import com.zhengkw.common.Constant
 import com.zhengkw.gmall.realtime.util.MyKafkaUtil
 import org.apache.spark.SparkConf
 import org.apache.spark.streaming.{Seconds, StreamingContext}
@@ -15,9 +16,12 @@ import org.apache.spark.streaming.{Seconds, StreamingContext}
 object DauApp {
   def main(args: Array[String]): Unit = {
     val conf = new SparkConf().setMaster("local[2]").setAppName("DauApp")
+    //一批3秒
     val ssc = new StreamingContext(conf, Seconds(3))
     //调用util里的kafka工具消费数据
- 
+    val ds = MyKafkaUtil.getKafkaStream(ssc, Constant.START_TOPIC)
+    //控制台上打印
+    ds.print(1000)
     ssc.start()
     ssc.awaitTermination()
   }
