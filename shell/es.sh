c2a40eacc0c2f8073a61faa33d29281a0eab565c
zhengkw <swimer@vip.qq.com>
2020-06-02 16:16:09 +0800

    common

--- shell/es.sh
+++ shell/es.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
-case $1 in "start")
+es_home=/zhengkw/module/elasticsearch-6.3.1
+log_home=/zhengkw/elklogs
+kibana_home=/zhengkw/module/kibana
+case $1 in
+"start")
 for host in hadoop102 hadoop103 hadoop104
 do
 	echo "==========启动$host ES========"
-	ssh $host "nohup /opt/module/elasticsearch-6.3.1/bin/elasticsearch 1>/opt/module/elasticsearch-6.3.1/start.log 2>/opt/module/elasticsearch-6.3.1/error.log &"
+	ssh $host "nohup $es_home/bin/elasticsearch 1>$log_home/start.log 2>$log_home/error.log &"
+    echo "==========启动$host kibana========"
+        nohup $kibana_home/bin/kibana 1> $log_home/kibanastart.log 2>$log_home/kibanastop.log &
+
 done
 xcall jps
 ;;
@@ -13,6 +20,9 @@ do
 	echo "==========关闭$host ES========"
 	ssh $host "jps | awk '/Elasticsearch/ {print \$1}' | xargs kill -9"
 done
+	echo "==========关闭hadoop102 Kibana========"
+ps -ef | grep node | grep -v grep|awk '{print $2}'|xargs kill -9
+
 xcall jps
 ;;
 *)
